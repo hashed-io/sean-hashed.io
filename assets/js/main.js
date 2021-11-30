568b9555c56c7de13c3551cf4be675279ac26ee2
DaniPoot <danielpoot.uc2@gmail.com>
2021-11-30 14:47:59 -0600

    Add functionality on navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 const $arrowLeft = document.querySelector('#arrow-left')
 const $arrowRight = document.querySelector('#arrow-right')
 const $container = document.querySelector('#projects-list')
+const $body = document.querySelector('body')
+
+const $checkbox = document.querySelector('.navigation-checkbox')
+const $navigationList = document.querySelector('.navigation-list')
+
+$checkbox.addEventListener('change', () => {
+  $body.classList.toggle('overflow-hide')
+})
+
+$navigationList.addEventListener('click', (e) => {
+  const { target } = e
+  const isLink = target.classList.contains('navigation-link')
+  if (isLink) {
+    const activeLink = document.querySelector('.navigation-link-active')
+    if (activeLink) activeLink.classList.toggle('navigation-link-active') 
+    target.classList.toggle('navigation-link-active')
+    $checkbox.checked = false
+    $body.classList.toggle('overflow-hide')
+  }
+})
 
 
 $arrowLeft.addEventListener('mousedown', () => {
